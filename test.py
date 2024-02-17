80b57710956dd92a405dc3aa653e1f56c40fe5a0
Pete Chao <pchaosm@gmail.com>
2024-02-17 10:32:29 -0800

    add and push test.py.

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-print("This is from local drive 1")
+print("This is from local drive a")
 print("This is ")
-print("This is from remote drive b")
+print("This is from remote drive 2")
+print("will this cause conflict")
