95e40101db49d0c31018f8a963a6683d32d25b83
Pete Chao <pchaosm@gmail.com>
2024-02-18 09:30:51 -0800

    add and push test.py- bc.

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
 print("This is from local drive a")
 print("This is from remote ")
 print("This is from remote A")
-print("This is from remote C")
-print("This is from remote B")
-print("This is from remote ")
