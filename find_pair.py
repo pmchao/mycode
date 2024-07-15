e72fada9707969afbb8c032e87b08239f2a0316d
Pete Chao <pchaosm@gmail.com>
2024-07-15 13:54:22 -0700

    This program find the pair that add to the target.

---
+++ find_pair.py
@@ -0,0 +1,18 @@
+"""
+Find pair with target sum
+"""
+
+def find_pairs_with_sum(numbers, target):
+    seen = set()
+    pairs = []
+    for number in numbers:
+        complement = target - number
+        if complement in seen:
+            pairs.append((number, complement))
+        seen.add(number)
+    return pairs
+
+# Example usage
+numbers = [2, 4, 3, 5, 7, 8, 1]
+target = 10
+print(find_pairs_with_sum(numbers, target))
