9851a9b5ea634c22a272a0679af91765f53a7f99
Pete Chao <pchaosm@gmail.com>
2024-08-20 14:00:51 -0700

    build 82002

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ This program  is used to test the following issues:
 (1) rebase in git
 (1a): continue
 """
-
 print("1","This is line 1")
 print("2","This is line 2")
 print("3","This is line 3")
-print("4","This is line 4")
+print("45","This is line 4")
+
 print("This concluded the program")
 print("=============================")
