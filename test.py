5cf8d27be71c63c1922a573f6ffdcc61e552854e
Pete Chao <pchaosm@gmail.com>
2024-08-22 09:17:05 -0700

    8-22-04

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ This program  is used to test the following issues:
 (1a): continue
 (2) rebase next lesson
 (3) dig deeper of rebase
+(4) rebase with the newest line
 """
 print("1","This is line 1")
 print("2","This is line 2")
