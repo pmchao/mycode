5ae08d1ec218c987c7d7d0fd56d49d72a7ca0ca4
Pete Chao <pchaosm@gmail.com>
2024-09-09 10:53:01 -0700

    commit for ver 1 and 2

--- test.py
+++ test.py
@@ -1,3 +1,1 @@
 print ("This is the simplified version 1")
-print ("This is the simplified version 2")
-print ("This is the simplified version 3")
--- test2.py
+++ test2.py
@@ -1,1 +1,1 @@
-print("This is the 2nd version for rest")
+print("This is the 2nd version for rest 3")
