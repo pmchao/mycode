8a95889bf55bbdce3e3f56d987856ccd03ce3bdc
Pete Chao <pchaosm@gmail.com>
2025-04-29 15:44:35 -0700

    add new arrange zero and one

--- arrange_zero_one.py
+++ arrange_zero_one.py
@@ -1,14 +1,25 @@
-#my_list = [1, 2, 3,1, 0,4, 5, 0]
-my_list2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+"""
+Reorganize a list such that:
+- All 0s are moved to the beginning (left)
+- All 1s are moved to the end (right)
+- All other numbers stay in the middle, preserving their original order
 
-# List comprehension to rearrange the list
-#processed_list = [0 for i in my_list if i == 0] + [i for i in my_list if i not in [0, 1]] + [1 for i in my_list if i == 1]
-#processed_list = [0 for i in my_list if i == 0]+ [i for i in my_list if i not in [0, 1]]
-#print(processed_list)
-divided_by_2_if_even = [x / 2 if x % 2 == 0 else x for x in my_list2]
+Example:
+Input : [1, 2, 3, 1, 0, 4, 5, 0]
+Output: [0, 0, 2, 3, 4, 5, 1, 1]
+"""
 
+# Original list
+my_list = [1, 2, 3, 1, 0, 4, 5, 0]
 
+# Reorganize the list as described above
+processed_list = (
+    [x for x in my_list if x == 0] +                # Zeros to the front
+    [x for x in my_list if x not in (0, 1)] +       # All others in the middle
+    [x for x in my_list if x == 1]                  # Ones to the end
+)
 
+# Display results
+print("Original list:", my_list)
+print("Processed list:", processed_list)
 
-
-print(divided_by_2_if_even)
